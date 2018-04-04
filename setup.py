a9b6cf8e9a03cf7e9dffab7b77fc8a3964d2575e
Sergey Zolotykh <szolotykh88@gmail.com>
2018-04-03 22:44:34 -0400

    Updated project name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='cyclopsmqtt',
+setup(name='cyclops-mqtt',
       version='0.1',
       description='Cyclops MQTT',
-      url='https://github.com/szolotykh/cyclopsmqtt',
+      url='https://github.com/vsreality/cyclops-mqtt',
       author='Sergey Zolotykh',
       author_email='milk3dfx@gmail.com',
       license='MIT',
-      packages=['cyclopsmqtt'],
+      packages=['cyclops-mqtt'],
       zip_safe=False)
